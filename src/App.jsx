d4a695a13fdc88eb92d37b31c3066bc7defca1fc
johurul-abir <mdjohurulislam555@gmail.com>
2024-01-21 13:38:50 +0600

    ddd

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { RouterProvider } from 'react-router-dom'
+import './App.scss';
+import router from './router/Router';
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
